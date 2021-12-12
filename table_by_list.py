661d3c6c80a7cac06089af33cbd25e4ba60a7835
SURAJ SAFI <94732511+suraj-safi1@users.noreply.github.com>
2021-12-12 12:40:51 +0530

    Table print by using list index

---
+++ table_by_list.py
@@ -0,0 +1,7 @@
+#print table of any integer by using list
+
+list=[1,2,3,4,5,6,7,8,9,10]
+num=int(input("Enter the number:\n"))
+print("table of ",num)
+print( list[0]*num,"\n",list[1]*num,"\n",list[2]*num,"\n",list[3]*num,"\n",list[4]*num,"\n",list[5]*num,"\n",list[6]*num,"\n",list[7]*num,"\n",list[8]*num,"\n",list[9]*num)
+
